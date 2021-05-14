5e964aca98d767b3cb507a4402ce5d6cf69be7dc
Wolfgang Liegel <wolfgang.liegel@openvalue.de>
2021-05-15 01:49:44 +0200

    Request access to local video device.

--- VideoChatDemo/src/app/chat/chat.component.ts
+++ VideoChatDemo/src/app/chat/chat.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+
+const mediaConstraints = {
+  audio: true,
+  video: {width: 1280, height: 720}
+  // video: {width: 1280, height: 720} // 16:9
+  // video: {width: 960, height: 540}  // 16:9
+  // video: {width: 640, height: 480}  //  4:3
+  // video: {width: 160, height: 120}  //  4:3
+};
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements AfterViewInit {
+
+  @ViewChild('local_video') localVideo: ElementRef;
+  @ViewChild('received_video') remoteVideo: ElementRef;
+
+  private localStream: MediaStream;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.requestMediaDevices();
   }
 
+  private async requestMediaDevices(): Promise<void> {
+    try {
+      this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+      // pause all tracks
+      this.pauseLocalVideo();
+    } catch (e) {
+      console.error(e);
+      alert(`getUserMedia() error: ${e.name}`);
+    }
+  }
+
+  pauseLocalVideo(): void {
+    console.log('pause local stream');
+    this.localStream.getTracks().forEach(track => {
+      track.enabled = false;
+    });
+    this.localVideo.nativeElement.srcObject = undefined;
+  }
 }
