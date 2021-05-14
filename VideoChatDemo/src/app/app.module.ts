ee4272a58bdfe20f78ef7a0e2fcb86953404982b
Wolfgang Liegel <wolfgang.liegel@openvalue.de>
2021-05-15 01:21:06 +0200

    added chat component

--- VideoChatDemo/src/app/app.module.ts
+++ VideoChatDemo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
